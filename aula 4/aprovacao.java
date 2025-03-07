e23e5af9dde076a6c133a9b3e1b1e95e63dd6710
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-03-07 11:21:39 -0300

    Create aprovacao.java

---
+++ aula 4/aprovacao.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+//maior menor numero
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        int matricula[] = {1234, 2345, 3456, 4567,5678};
+        String nome[] = {"Arthur","Becca","Bruna","Matheus","Victor"};
+        int[][] notas = new int[5][5];
+        
+        
+        // notas[i][j]
+        
+        
+        
+        nota =[
+                                    [7,8,9,10,6]
+                                    [6,7,8,9,10]
+                                    [10,8,9,6,7]
+                                        [9,8,7,10,6]
+                           [9,10,8,6,7]
+        ];
+        float medias[];
+
+
+
+
+        leitor.close();
+    }
+}
