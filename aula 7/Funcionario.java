e8323e3cd60838c1d52eeb379dc8472e5b0e956a
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-03-20 11:16:56 -0300

    Create Funcionario.java

---
+++ aula 7/Funcionario.java
@@ -0,0 +1,17 @@
+public class Funcionario {
+    private String nome;
+    private String cargo;
+    private double salario;
+
+    public String getNome(String nome) {
+        return nome;
+    }
+
+    public String getCargo(String cargo) {
+        return cargo;
+    }
+
+    public double getSalario(double salario) {
+        return salario;
+    }
+}
