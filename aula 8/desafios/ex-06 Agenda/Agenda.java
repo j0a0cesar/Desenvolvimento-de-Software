bfebf42393ac72f3f443eb5607fe4d32e8e6c7e5
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-03-24 19:21:33 -0300

    Update Agenda.java

--- aula 8/desafios/ex-06 Agenda/Agenda.java
+++ aula 8/desafios/ex-06 Agenda/Agenda.java
@@ -8,15 +8,17 @@ public class Agenda {
     }
 
     public Contato buscarContato(String nomeProcurado) {
-
+// o for-each que a cada ciclo, contato recebe o valor do próximo elemento 
+//ele é bom menos propenso a erros, não tem o risco de estourar o índice, não precisa da condicional i < x, tambem não precisa i++   
         for (Contato contato : contatos) {
             if (contato.getNome().equalsIgnoreCase(nomeProcurado)) {
+                //comparação entre duas strings ignorando se as letras são maiúsculas ou minúsculas
                 return contato;
             }
         }
             System.out.println("Contato não encontrado!");
             return null;
-    }
+    }//aqui um metodo usando boolean, que tem que retorna true ou false
     public boolean removerContato(String nome) {
         Contato c = buscarContato(nome);
         if(c != null) {
@@ -26,3 +28,10 @@ public class Agenda {
         return false;
     }
 }
+/*Comentarios apos resover este exercicio:
+arrayList é novidade,  aprendi para usar aqui ArrarList é flexivel(não tem um tamnho definido) e é bom quando precisa adionar e remover com frequencia,
+e também tem metodos que facilitam o trabalho como .add(), .remove()
+ele foi o diferncial desse exercicio pois com ele mudou a forma de resolver a questão,diria que a logica da resolução, pois eu tinha pensado em fazer um arry comun, e uma busca sequncial,
+uma forma mais proxima do que se faz em C,
+ele mudou a  forma de adiconar e remover o contato tambem
+*/
