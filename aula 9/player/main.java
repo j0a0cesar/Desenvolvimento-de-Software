16889e7f59af4efaad56f368aa5eafcaa26863e8
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-02 20:27:10 -0300

    Update main.java

--- aula 9/player/main.java
+++ aula 9/player/main.java
@@ -6,21 +6,23 @@ public class Main {
     public static void main(String[] args) {
 
         List<Player> jogadores = new ArrayList<>();
-        jogadores.add(new Player(1,2));
-        jogadores.add(new Player(4,4));
+        jogadores.add(new Player(1, 2));
+        jogadores.add(new Player(4, 4));
 
         for (int linha = 0; linha < 5; linha++) {
             for (int coluna = 0; coluna < 5; coluna++) {
 
-                for(int i = 0;i < jogadores.size(); i++) {
-                    if (linha == jogadores.get(i).x && coluna == jogadores.get(i).y) {
+                for (int i = 0;i < jogadores.size(); i++) {
+                    if (jogadores.get(i).x == linha && jogadores.get(i).y == coluna) {
                         System.out.print("[ X ]");
-                    } else {
-                        System.out.print("[ . ]");
                     }
                 }
+                System.out.print("[ . ]");
 
             }
             System.out.println();
-        }
-    }
+        }
+
+    }
+
+}
