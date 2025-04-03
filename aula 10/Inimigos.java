85f5918f85126c90b0388f2b808de8f331c323b9
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-03 11:11:53 -0300

    Create Inimigos.java

---
+++ aula 10/Inimigos.java
@@ -0,0 +1,6 @@
+public class Inimigos extends Persongagem {
+
+    public Inimigos(int x, int y, String desenho) {
+        super(x, y, desenho);//vai mandar para construtor da classe de cima, no caso da classe personagem
+    }
+}
