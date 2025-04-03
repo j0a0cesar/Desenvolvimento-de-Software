db81f79328aa83290fcf2329a5bb8f7c70035ff4
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-03 11:11:03 -0300

    Create Personagagem.java

---
+++ aula 10/Personagagem.java
@@ -0,0 +1,11 @@
+public class Persongagem {
+    int x;
+    int y;
+    String desenho;
+    public Persongagem(int x, int y,String desenho){
+        this.x = x;
+        this.y = y;
+        this.desenho = desenho;
+    }//tem que ser privado
+    //ter os gets e os sets
+}
