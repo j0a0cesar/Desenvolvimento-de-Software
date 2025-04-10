f91b0c4cc828ea64ece1028d8465943a5e2f93d8
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-10 10:58:34 -0300

    Create Pessoa.java

---
+++ aula 11/desafio/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa {
+    private String nome;
+    private int cpf;
+    private int idade;
+
+    public void mostrarNome(){
+        System.out.println(this.nome);
+    }
+    public void mostrarCPF(){
+        System.out.println(this.cpf);
+    }
+    public void mostrarIdade(){
+        System.out.println(this.idade);
+    }
+}
