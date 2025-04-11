604acbcff60bb7d0097a2fa919f3565869663978
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-11 10:24:10 -0300

    Update Verdedor.java

--- aula 11/desafio/Verdedor.java
+++ aula 11/desafio/Verdedor.java
@@ -1,3 +1,16 @@
-public class Verdedor extends Funcionario{
-    String venda;
+public class Vendendor extends Funcionario{
+    float meta;
+    float vendas;
+
+    Vendendor(String nome, String cpf, int idade,String cargo,int id,float salario,float meta,float vendas) {
+        super(nome, cpf, idade, cargo, id, salario);
+        this.meta = meta;
+        this.vendas=vendas;
+    }
+    void vender(){
+        System.out.println("Gerente: "+this.nome);
+        System.out.println("| Tem a meta de vender: "+this.meta);
+        System.out.println("| Vendeu: "+this.vendas);
+
+    }
 }
