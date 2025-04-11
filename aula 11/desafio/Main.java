053d217980a55a03b2413107d5e69d7cc67debaf
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-11 10:23:46 -0300

    Update Main.java

--- aula 11/desafio/Main.java
+++ aula 11/desafio/Main.java
@@ -1,16 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Pessoa p = new Pessoa();
 
 
-        Funcionario f = new Funcionario();
+        public class Main {
+            public static void main(String[] args) {
 
-        Gerente g = new Gerente();
-
-        Verdedor v = new Verdedor();
-
+                Pessoa p = new Pessoa("joão","0",19);
 
 
-    }
-}
+                Funcionario f = new Funcionario("Zé","2",18,"estagiario",001,1200);
+
+
+                Gerente g = new Gerente("Luiz","3",48,"gerente",002,12000,5);
+
+                Vendendor v = new Vendendor("Rafa","4",38,"vendedor",003,7500,1200,1500);
+                v.trabalhar();
+                v.vender();
+
+                Cliente c = new Cliente("Maria","1",21);
+
+                g.liderar();
+                g.trabalhar();
+
+            }
+        }
+
