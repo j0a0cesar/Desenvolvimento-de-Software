64c82d71b52bbe121d0ef9f6f7700d70c950e3ba
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-11 10:26:08 -0300

    Update Gerente.java

--- aula 11/desafio/Gerente.java
+++ aula 11/desafio/Gerente.java
@@ -1,3 +1,16 @@
 public class Gerente extends Funcionario {
     String manda;
+    int tamanhoEquipe;
+
+    Gerente(String nome, String cpf, int idade,String cargo,int id,float salario, int tamanhoEquipe) {
+        super(nome, cpf, idade, cargo, id, salario);
+        this.tamanhoEquipe = tamanhoEquipe;
+
+    }
+    void liderar(){
+        System.out.println("Gerente: "+this.nome);
+        System.out.println("| Está liderando uma equipe de: "+this.tamanhoEquipe);
+        System.out.println("| Ganhando salario R$: "+this.salario);
+        System.out.println("| No cargo: "+this.cargo);
+    }
 }
