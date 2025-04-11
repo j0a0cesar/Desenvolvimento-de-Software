57386a914380071f2731b301949c861bb95de3c6
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-11 10:24:27 -0300

    Update Pessoa.java

--- aula 11/desafio/Pessoa.java
+++ aula 11/desafio/Pessoa.java
@@ -1,15 +1,23 @@
 public class Pessoa {
-    private String nome;
-    private int cpf;
-    private int idade;
 
-    public void mostrarNome(){
-        System.out.println(this.nome);
-    }
-    public void mostrarCPF(){
-        System.out.println(this.cpf);
-    }
-    public void mostrarIdade(){
-        System.out.println(this.idade);
-    }
+         String nome;
+         String cpf;
+         int idade;
+
+        Pessoa(String nome,String cpf,int idade){
+            this.nome=nome;
+            this.cpf=cpf;
+            this.idade=idade;
+        }
+
+        public void mostrarNome(){
+            System.out.println(this.nome);
+        }
+        public void mostrarCPF(){
+            System.out.println(this.cpf);
+        }
+        public void mostrarIdade(){
+            System.out.println(this.idade);
+        }
+
 }
