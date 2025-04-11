c46e449a8b0f15cb708b3241e9fb84e01ba16a74
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-04-11 10:27:03 -0300

    Update Funcionario.java

--- aula 11/desafio/Funcionario.java
+++ aula 11/desafio/Funcionario.java
@@ -1,5 +1,20 @@
 public class Funcionario extends Pessoa{
-    private String setor;
-    private int id;
-    private int salario;
+
+
+     String cargo;
+     int id;
+     float salario;
+
+    Funcionario(String nome, String cpf, int idade,String cargo,int id, float salaraio) {
+        super(nome, cpf, idade);
+        this.cargo = cargo;
+        this.id = id;
+        this.salario = salaraio;
+    }
+
+    void trabalhar(){
+        System.out.println("| "+this.nome);
+        System.out.println("~ Trabalhando com "+this.cargo);
+        System.out.println("~ Ganhando R$ "+this.salario);
+    }
 }
