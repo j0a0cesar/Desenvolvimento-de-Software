df08f67191930e5ce844945f4cd2fe32df2d745f
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-05-15 09:49:14 -0300

    Create Calculadora.java

---
+++ aula 13/Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora {
+
+    public static int soma (int a, int b){
+        return (a+b);
+    }
+    public static float soma(float n1, float n2){
+        return (n1+ n2);
+    }
+    public static float soma(float n1, float n2, float n3){
+        return (n1+ n2 + n3);
+    }
+    public static float soma(float n1, float n2, float n3, float n4){
+        return (n1+ n2+ n3 + n4);
+    }
+}
