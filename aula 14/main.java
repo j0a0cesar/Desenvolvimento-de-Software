2d8e4f3d761211b7d7cf0763742d398aed7a7802
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-05-16 10:56:42 -0300

    Update and rename tratamento de erro to main.java

---
+++ aula 14/main.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("Digite um nome: ");
+            String nome = scan.nextLine();
+
+
+            int idade = InputHelp.pegarNumero("idade: ");
+            int peso = InputHelp.pegarNumero("peso: ");
+            /*try {
+                System.out.print("Digite uma idade: ");
+
+                //int idade = scan.nextInt();
+                //scan.nextLine();
+
+
+            }
+            catch (NumberFormatException e){
+                //catch (Exception e){  //pega todos os erros
+                //throw new RuntimeException(e);
+                //System.out.println(e);
+                System.out.println("Digite apenas numeros!");
+                System.out.println("Digite novamente");
+            }catch (Exception e){
+                System.out.println(" > > > ERRO < < <");
+            }*/
+
+
+        }
+    }
+}
