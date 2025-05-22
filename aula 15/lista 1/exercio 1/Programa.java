e90728ce48c0a4355c3141a24c07cb8b778ede75
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-05-22 17:24:57 -0300

    Update Programa.java

--- aula 15/lista 1/exercio 1/Programa.java
+++ aula 15/lista 1/exercio 1/Programa.java
@@ -1,5 +1,8 @@
 public class Programa {
     public static void main(String[]args) {
+        //Crie uma classe que represente um Livro de uma biblioteca. Em outra classe
+        //(Programa), instancie os seguintes livros e apresente seus dados na tela.
+        //(Não precisa usar Model-View-Controller MVC)
 
         Livro livro1 = new Livro("001","Iracema", "José de Alencar","9788542221138",1865);
         Livro livro2= new Livro("002","Cortiço", "Aluíso de Azevedo","9788542221139",1890);
