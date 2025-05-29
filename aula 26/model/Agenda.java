255182e0f2a3acf8138f1a6d0ce545a7b5d7426f
João César <164185005+j0a0cesar@users.noreply.github.com>
2025-05-29 20:06:23 -0300

    Create Agenda.java

---
+++ aula 26/model/Agenda.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.*;
+
+public class Agenda {
+    private static List<Contato> contatos = new ArrayList<>();
+
+    public static void adicionar(Contato contato) {
+        contatos.add(contato);
+    }
+
+    public static Contato getContato(int codigo) {
+        for (Contato c : contatos) {
+            if (c.getCodigo() == codigo) return c;
+        }
+        return null;
+    }
+
+    public static boolean excluirContato(int codigo) {
+        return contatos.removeIf(c -> c.getCodigo() == codigo);
+    }
+
+    public static List<Contato> listarContatos() {
+        return contatos;
+    }
+}
